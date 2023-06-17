1799ef54ee160227448807bd180a5213d991b935
Abdullah Sultan <82256525+Abdullah-9862873@users.noreply.github.com>
2023-06-17 16:39:13 +0500

    Completed Flow Of Program Questions

---
+++ Flow of Program/SumOfNumbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SumOfNumbers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int num1 = input.nextInt();
+        System.out.println("Enter second number: ");
+        int num2 = input.nextInt();
+        input.close();
+
+        int sum = num1 + num2;
+        System.out.println("Sum is: " + sum);
+    }
+}
